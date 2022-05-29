e287ea624399858849a0ec129bd6ae03c7e28a7c
tsemkaloalena-mirea <tsemkaloalena@gmail.com>
2022-05-29 15:20:32 +0300

    Home page

--- TravelCompanyApplication/app/src/main/java/com/example/travelcompanyapplication/ui/HomeFragment.java
+++ TravelCompanyApplication/app/src/main/java/com/example/travelcompanyapplication/ui/HomeFragment.java
@@ -10,10 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.example.travelcompanyapplication.FlightsViewBuilder;
+import com.example.travelcompanyapplication.MainActivity;
 import com.example.travelcompanyapplication.R;
+import com.example.travelcompanyapplication.databinding.FragmentFlightsBinding;
 import com.example.travelcompanyapplication.databinding.FragmentHomeBinding;
+import com.example.travelcompanyapplication.db_controller.TravelCompanyDBHelper;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     private View view;
     private FragmentHomeBinding binding;
 
@@ -21,7 +25,31 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        Button guideButton = (Button) view.findViewById(R.id.guide_page_button);
+        guideButton.setOnClickListener(this);
+        Button hotelButton = (Button) view.findViewById(R.id.hotels_page_button);
+        hotelButton.setOnClickListener(this);
+        Button flightButton = (Button) view.findViewById(R.id.flights_page_button);
+        flightButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.guide_page_button:
+                Navigation.findNavController(view).navigate(R.id.navigation_guide);
+
+                break;
+            case R.id.hotels_page_button:
+                Navigation.findNavController(view).navigate(R.id.navigation_hotels);
+                break;
+            case R.id.flights_page_button:
+                Navigation.findNavController(view).navigate(R.id.navigation_flights);
+                break;
+        }
     }
 
     @Override
